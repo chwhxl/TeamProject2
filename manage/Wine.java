775cf15620bcb1be460ba101b50beeeb7dd2e93f
Song Jumi <jumisong2002@naver.com>
2025-11-27 16:00:48 +0900

    Add files via upload

---
+++ manage/Wine.java
@@ -0,0 +1,74 @@
+package manage;
+
+enum WineType {
+    RED, WHITE, SPARKLING
+}
+
+enum WineCountry {
+    FRANCE, ITALY, SPAIN
+}
+
+public class Wine {
+
+    protected String name; // 상품명
+    protected int price; // 가격
+    protected WineType type; // 종류
+    protected WineCountry country; // 원산지
+    protected String winery; // 생산 와이너리
+    protected String grape; // 포도 품종
+    protected double alcohol; // 알코올 도수
+    protected int year; // 생산 연도
+    protected int stock; // 재고 수량
+
+    public Wine(String name, int price, WineType type, WineCountry country, String winery, String grape, double alcohol, int year) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.country = country;
+        this.winery = winery;
+        this.grape = grape;
+        this.alcohol = alcohol;
+        this.year = year;
+        this.stock = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public WineType getType() {
+        return type;
+    }
+
+    public WineCountry getCountry() {
+        return country;
+    }
+
+    public String getWinery() {
+        return winery;
+    }
+
+    public String getGrape() {
+        return grape;
+    }
+
+    public double getAlcohol() {
+        return alcohol;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        // 상품 상세 정보 출력
+        return "";
+    }
+
+
+}
---
+++ manage/cartPanel.java
@@ -0,0 +1,69 @@
+package manage;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import manage.cart;
+import manage.Wine;
+
+public class cartPanel extends JPanel{
+	MainFrame mainFrame;
+	JTextArea cartArea;
+	
+	
+	public void CartPanel(MainFrame mainFrame) {
+		this.mainFrame = mainFrame;
+		setLayout(new BorderLayout());
+		
+		JLabel title = new JLabel("🛒 내 장바구니", SwingConstants.LEFT);
+		title.setFont(new Font ("맑은 고딕", Font.BOLD, 20));
+		add(title, BorderLayout.NORTH);
+		
+		cartArea = new JTextArea();
+		cartArea.setEditable(false);
+		add(new JScrollPane(cartArea),BorderLayout.WEST);
+		
+		JPanel btnPanel = new JPanel();
+		JButton btnBack = new JButton("뒤로가기");
+		JButton btnCheckOut = new JButton("결제하기");
+		
+		btnBack.addActionListener(new MyActionListener());		
+		btnCheckOut.addActionListener(new MyActionListener());
+		
+		btnPanel.add(btnCheckOut);
+		btnPanel.add(btnBack);
+		add(btnPanel, BorderLayout.SOUTH);
+		
+	}
+	
+	private class MyActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if (b.getText().equals("뒤로가기")) {
+				mainFrame.showCard("CART");
+			}else if (b.getText().equals("결제하기")) {
+				mainFrame.showCard("CHECKOUT");
+			}
+		}
+		
+	}
+	
+	/*public void showCARD(String name){
+	 * if (name.equals("CART")){
+	 * cartPa*/
+	public void updateCartList() {
+		cartArea.setText("");
+		int total = 0;
+		
+		cartArea.append("상품명 | 가격\n");
+		cartArea.append("_____________________________________\n");
+		
+		for (Wine w : cart.itemsInCart) {
+			cartArea.append(w.getName() + " | " + w.getPrice() + "원 \n" );
+			total += w.getPrice();
+		}
+		
+		cartArea.append("_______________________________________\n");
+		cartArea.append("총 합계: "+ total + "원");
+	}
+}
