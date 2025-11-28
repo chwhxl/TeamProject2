111e79da41b457cb96e8b4f32e65be6659a9ebd1
Song Jumi <jumisong2002@naver.com>
2025-11-29 02:49:54 +0900

    Add files via upload

---
+++ mallmain/Main.java
@@ -0,0 +1,54 @@
+package mallmain;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main extends JFrame {
+	
+	private CardLayout cardLayout;
+    private JPanel mainContainer;
+    
+    private HomePanel homePanel;
+    private ShopPanel winePanel;
+    private ShopPanel beerPanel;
+    private ShopPanel whiskeyPanel;
+    
+    public Main() {
+    	// JFrame 기본 설정
+    	
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫기 시 프로그램 종료
+        setTitle("Mall Main"); // 창 제목 설정
+        setSize(1600, 900); // MainFrame 크기 1600 x 900
+        setResizable(false);
+        
+        // CardLayout 설정
+        cardLayout = new CardLayout();
+        mainContainer = new JPanel(cardLayout);
+
+        // 1. 패널들을 미리 생성 -> 생성자에 'this'를 넘기는 이유는 패널들이 메인 프레임에게 화면 전환 요청하기 위함
+        homePanel = new HomePanel(this);
+        winePanel = new ShopPanel(this, "와인 상점", new Color(255, 240, 240)); // 연한 빨강 배경
+        beerPanel = new ShopPanel(this, "맥주 상점", new Color(255, 255, 224)); // 연한 노랑 배경
+        whiskeyPanel = new ShopPanel(this, "위스키 상점", new Color(240, 248, 255)); // 연한 파랑 배경
+
+        // 2. CardLayout에 패널들 등록 (이름표를 붙여줌)
+        mainContainer.add(homePanel, "HOME");
+        mainContainer.add(winePanel, "WINE");
+        mainContainer.add(beerPanel, "BEER");
+        mainContainer.add(whiskeyPanel, "WHISKEY");
+
+        add(mainContainer);
+        setVisible(true);
+    }
+
+    // 화면 전환을 담당하는 메서드
+    public void showCard(String cardName) {
+        cardLayout.show(mainContainer, cardName);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Main());
+    }
+
+}
