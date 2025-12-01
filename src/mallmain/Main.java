63bed5a8022bd3b21d537bdbe39fc2e58e4f733d
cvlab308 <cvlab308@DESKTOP-THRRVBS>
2025-12-02 02:37:46 +0900

    icon images

--- src/mallmain/Main.java
+++ src/mallmain/Main.java
@@ -23,7 +23,7 @@ public class Main extends JFrame {
     	// JFrame 기본 설정
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫기 시 프로그램 종료
         setTitle("Mall Main"); // 창 제목 설정
-        setSize(1600, 900); // MainFrame 크기 1600 x 900
+        setSize(1280, 720); // MainFrame 크기 1600 x 900
         setResizable(false);
         
         // CardLayout 설정
