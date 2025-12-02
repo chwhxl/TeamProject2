23358c739bdb6a03235cff712270d1318e12b276
Jumi Song <jumisong2002@naver.com>
2025-12-02 21:29:42 +0900

    line align

--- src/wineshop/WineSearchResult.java
+++ src/wineshop/WineSearchResult.java
@@ -1,6 +1,9 @@
 package wineshop;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import mallmain.Main;
 
@@ -16,7 +19,7 @@ public class WineSearchResult extends JPanel{
         this.wine = wine;
 
         // 카드 디자인
-        setPreferredSize(new Dimension(180, 280));
+        setPreferredSize(new Dimension(180, 290));
         setBorder(BorderFactory.createLineBorder(Color.GRAY));
         setLayout(new BorderLayout());
 
@@ -43,19 +46,33 @@ public class WineSearchResult extends JPanel{
         imgLabel.setPreferredSize(new Dimension(100, 180));
         add(imgLabel, BorderLayout.NORTH);
         
-        // 상품명 / 가격 / 도수
-        JPanel infoPanel = new JPanel(new GridLayout(3, 1));
-        JLabel name = new JLabel(wine.getName(), JLabel.CENTER);
-        DecimalFormat df = new DecimalFormat("#,###");
-        String formattedPrice = df.format(wine.getPrice());
-        JLabel price = new JLabel(formattedPrice + "원", JLabel.CENTER);
-        JLabel alc = new JLabel(wine.getAlcohol() + "%", JLabel.CENTER);
+        JPanel infoPanel = new JPanel(new BorderLayout(0, 5));
+	    JTextPane namePane = new JTextPane();
+	
+	    namePane.setText(wine.getName());
+	    StyledDocument doc = namePane.getStyledDocument();
+	    SimpleAttributeSet center = new SimpleAttributeSet();
+	    StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+	    doc.setParagraphAttributes(0, doc.getLength(), center, false);
+	
+	    namePane.setEditable(false);
+	    namePane.setOpaque(false); // 배경 투명하게
+	    namePane.setBorder(null); // 테두리 없애기
+	    namePane.setFont(new Font("맑은 고딕", Font.BOLD, 14)); // 폰트 설정
+	    JPanel bottomInfo = new JPanel(new GridLayout(2, 1));
+	    bottomInfo.setOpaque(false);
+	
+	    DecimalFormat df = new DecimalFormat("#,###");
+	    JLabel price = new JLabel(df.format(wine.getPrice()) + "원", JLabel.CENTER);
+	    JLabel alc = new JLabel(wine.getAlcohol() + "%", JLabel.CENTER);
+		
+	    bottomInfo.add(price);
+        bottomInfo.add(alc);
+
+        infoPanel.add(namePane, BorderLayout.CENTER);	
+	    infoPanel.add(bottomInfo, BorderLayout.SOUTH);
+	    add(infoPanel, BorderLayout.CENTER);
         
-        infoPanel.add(name);
-        infoPanel.add(price);
-        infoPanel.add(alc);
-        
-        add(infoPanel, BorderLayout.CENTER);
         
         JButton cartBtn = new JButton("장바구니에 추가");
         cartBtn.setFont(new Font("Noto Sans KR", Font.BOLD, 15));      
